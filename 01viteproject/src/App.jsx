d180b0bd308f0a3260882ee6dc63dbb88ef7f78d
Mohd Rafey <mohdrafey600@gmail.com>
2024-03-17 01:49:14 +0530

    practice

---
+++ 01viteproject/src/App.jsx
@@ -0,0 +1,7 @@
+import Hello from './hello';
+
+function App() {
+    return <Hello />;
+}
+
+export default App;
---
+++ 01viteproject/src/hello.jsx
@@ -0,0 +1,4 @@
+function Hello() {
+    return <h3>Hello Sir kaise hain</h3>;
+}
+export default Hello;
